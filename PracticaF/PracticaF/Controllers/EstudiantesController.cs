e3cc0921be0ae92b7fb1aa77f73dc5cbce4b1fc5
Ivanrosa172100@outlook.com <Ivanrosa172100@outlook.com>
2019-11-14 21:56:28 -0400

    Se creo la fase dos de la tarea, se consulta las materias de lo estudiantes registrados, con sus respectivos controles y vistas.

---
+++ PracticaF/PracticaF/Controllers/EstudiantesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PracticaF.Models;
+
+namespace PracticaF.Controllers
+{
+    public class EstudiantesController : Controller
+    {
+        private PracticaFEntities db = new PracticaFEntities();
+
+        // GET: Estudiantes
+        public ActionResult Index()
+        {
+            return View(db.Estudiante.ToList());
+        }
+
+        // GET: Estudiantes/Details/5
+        public ActionResult Details(int? id)
+        {
+            var lista = from data in db.CursosEstudiantes
+                        where data.IdEstudiante == id
+                        select data;
+            return View(lista);
+        }
+
+        // GET: Estudiantes/Create
+       
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
