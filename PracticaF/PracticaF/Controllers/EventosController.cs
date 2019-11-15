4f2dc6d6a29ec371a18f95dbe79a658bcba127d8
Ivanrosa172100@outlook.com <Ivanrosa172100@outlook.com>
2019-11-15 16:15:44 -0400

    Se agrego la funcionalidad de agenda evento con sus repectivos controladores y vistas.

---
+++ PracticaF/PracticaF/Controllers/EventosController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PracticaF.Models;
+
+namespace PracticaF.Controllers
+{
+    public class EventosController : Controller
+    {
+        private PracticaFEntities db = new PracticaFEntities();
+
+        // GET: Eventos
+        public ActionResult Index(String busqueda )
+        {
+            var Lista = from data in db.Evento select data;
+            if (string.IsNullOrEmpty(busqueda))
+            {
+                return View(db.Evento.ToList());
+            }
+            else if (busqueda != "l")
+            {
+                Lista = Lista.Where(a => a.evento1.Contains(busqueda));
+                return View(Lista);
+
+            }
+            else
+            {
+                Lista = Lista.Where(a => a.Fecha.Equals(busqueda));
+                return View(Lista);
+            }
+        }
+
+        // GET: Eventos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Evento evento = db.Evento.Find(id);
+            if (evento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(evento);
+        }
+
+        // GET: Eventos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Eventos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,evento1,Fecha,Hora")] Evento evento)
+        {
+            if (ModelState.IsValid)
+            {
+               
+                db.Evento.Add(evento);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(evento);
+        }
+
+        // GET: Eventos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Evento evento = db.Evento.Find(id);
+            if (evento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(evento);
+        }
+
+        // POST: Eventos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,evento1,Fecha,Hora")] Evento evento)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(evento).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(evento);
+        }
+
+        // GET: Eventos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Evento evento = db.Evento.Find(id);
+            if (evento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(evento);
+        }
+
+        // POST: Eventos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Evento evento = db.Evento.Find(id);
+            db.Evento.Remove(evento);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
