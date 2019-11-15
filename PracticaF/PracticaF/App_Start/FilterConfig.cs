3e286d785af874baec9eefe4ddcd1e35b3e416cc
Ivanrosa172100@outlook.com <Ivanrosa172100@outlook.com>
2019-11-14 21:30:11 -0400

    Se creo el Proyecto se agregaron los modelos de datos y la autenticacion con facebook.

---
+++ PracticaF/PracticaF/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PracticaF
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
