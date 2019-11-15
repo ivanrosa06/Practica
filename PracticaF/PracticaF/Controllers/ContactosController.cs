1a2c943aa580425df701c89c6f7941cd8f89a216
Ivanrosa172100@outlook.com <Ivanrosa172100@outlook.com>
2019-11-14 23:07:26 -0400

    Se realizo la funcionalidad de agenda de contactos con sus respectivos controladores y vistas

---
+++ PracticaF/PracticaF/Controllers/ContactosController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PracticaF.Models;
+
+namespace PracticaF.Controllers
+{
+    public class ContactosController : Controller
+    {
+        private PracticaFEntities db = new PracticaFEntities();
+
+        // GET: Contactos
+        public ActionResult Index(String busqueda)
+        {
+            var Lista = from data in db.Contactos select data;
+            if (string.IsNullOrEmpty(busqueda))
+            {
+                return View(db.Contactos.ToList());
+            }
+            else if (busqueda != "l")  {
+                Lista = Lista.Where(a => a.celular.Contains(busqueda));
+                return View(Lista);
+
+            }
+            else
+            {
+                Lista = Lista.Where(a => a.nombre.Contains(busqueda));
+                return View(Lista);
+            }
+
+        }
+
+        // GET: Contactos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contactos contactos = db.Contactos.Find(id);
+            if (contactos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactos);
+        }
+
+        // GET: Contactos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contactos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,nombre,celular,email,direccion")] Contactos contactos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contactos.Add(contactos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contactos);
+        }
+
+        // GET: Contactos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contactos contactos = db.Contactos.Find(id);
+            if (contactos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactos);
+        }
+
+        // POST: Contactos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,nombre,celular,email,direccion")] Contactos contactos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contactos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contactos);
+        }
+
+        // GET: Contactos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contactos contactos = db.Contactos.Find(id);
+            if (contactos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactos);
+        }
+
+        // POST: Contactos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contactos contactos = db.Contactos.Find(id);
+            db.Contactos.Remove(contactos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
